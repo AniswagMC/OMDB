755ea843920fa6ad14b48f50edbe829e2f83b813
AniswagMC <anirudh.duggal.ad.ad@gmail.com>
2022-10-22 17:21:38 -0700

    Mad changes being made. Poster is now seen on search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 import Button from './Button';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState({});
+
+  // console.log(data)
+  
+  const searchResult = (childData) => {
+    setData(childData)
+  }
+
   return (
     <div className="App">
-      <Button title = "Just do it" />
+      <input placeholder='Search Movies' onChange={e => setSearch(e.target.value)} />
+      <Button title = "Search Movie Name" query = {search} queryCallBack={searchResult} />
+      <img src={data['Poster']} />
     </div>
   );
 }
